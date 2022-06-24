968a28c2b2c6055357b75ca67b3679ac6cfdf01e
MoneyAdd <tjdwns0158@naver.com>
2022-06-25 00:24:54 +0900

    인간승리

--- app/src/main/java/com/cookandroid/myfintech/CafePoint.java
+++ app/src/main/java/com/cookandroid/myfintech/CafePoint.java
@@ -2,13 +2,29 @@ package com.cookandroid.myfintech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CafePoint extends AppCompatActivity {
+
+    private ImageView imgleft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe_point);
+
+        imgleft = findViewById(R.id.imgleft);
+
+        imgleft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CafePoint.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

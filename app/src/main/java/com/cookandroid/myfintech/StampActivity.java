38f1542ab6c8e8cf4a6a23d003d71763093e5ed8
user <shinage2@naver.com>
2022-06-24 22:52:43 +0900

    xml추가

---
+++ app/src/main/java/com/cookandroid/myfintech/StampActivity.java
@@ -0,0 +1,16 @@
+package com.cookandroid.myfintech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StampActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stamp);
+
+
+    }
+}
